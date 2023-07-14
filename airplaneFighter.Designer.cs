7e4888309c382810186e63aa6cb60dca0c37722c
Alex Cetanas <116481552+Alex-cet@users.noreply.github.com>
2023-07-14 19:42:50 +0300

    Add files via upload

---
+++ airplaneFighter.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Airplane_Fighter
+{
+    partial class airplaneFighter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(airplaneFighter));
+            this.startGameButton = new System.Windows.Forms.Button();
+            this.airCraft = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.airCraft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startGameButton
+            // 
+            this.startGameButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.startGameButton.BackColor = System.Drawing.Color.White;
+            this.startGameButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startGameButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.startGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startGameButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.startGameButton.Location = new System.Drawing.Point(193, 130);
+            this.startGameButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.startGameButton.Name = "startGameButton";
+            this.startGameButton.Size = new System.Drawing.Size(400, 100);
+            this.startGameButton.TabIndex = 0;
+            this.startGameButton.Text = "Start Game";
+            this.startGameButton.UseMnemonic = false;
+            this.startGameButton.UseVisualStyleBackColor = false;
+            this.startGameButton.Click += new System.EventHandler(this.startGameButton_Click);
+            // 
+            // airCraft
+            // 
+            this.airCraft.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.airCraft.BackColor = System.Drawing.Color.Transparent;
+            this.airCraft.Image = ((System.Drawing.Image)(resources.GetObject("airCraft.Image")));
+            this.airCraft.Location = new System.Drawing.Point(400, 390);
+            this.airCraft.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.airCraft.Name = "airCraft";
+            this.airCraft.Size = new System.Drawing.Size(50, 50);
+            this.airCraft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.airCraft.TabIndex = 1;
+            this.airCraft.TabStop = false;
+            this.airCraft.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.airCraft);
+            this.Controls.Add(this.startGameButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.airCraft)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startGameButton;
+        private System.Windows.Forms.PictureBox airCraft;
+    }
+}
+
---
+++ airplaneFighter.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Airplane_Fighter
+{
+    public partial class airplaneFighter : Form
+    {
+        private bool stopThread = false;
+        private bool stopMoveThread = false;
+        private PictureBox[] asteroids;
+        private TextBox score;
+        private int counter = -1;
+        public airplaneFighter()
+        {
+            InitializeComponent();
+            this.MaximumSize = new Size(800, 500);
+            this.WindowState = FormWindowState.Maximized;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.KeyPreview = true;
+            asteroids = new PictureBox[1000];
+        }
+
+        private void startGameButton_Click(object sender, EventArgs e)
+        {
+            startGameButton.Visible = false;
+            airCraft.SetBounds(400, 410, 40 ,40);
+            airCraft.Visible = true;
+            
+
+            Thread thread = new Thread(generateAsteroids);
+            thread.Start();
+            generateTimer();
+            Thread timer = new Thread(updateTimer);
+            timer.Start();
+           
+        }
+        private void generateTimer() {
+            this.Invoke(new Action(() => {
+
+            score = new TextBox();
+            score.Text = "Score: 0";
+            score.Location = new Point(300, 10);
+            score.Size = new Size(200, 20);
+            score.Visible = true;
+            score.Enabled = false;
+            this.Controls.Add(score);
+            }));
+        }
+
+        private void updateTimer() {
+            while (!stopThread)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    ++counter;
+                    score.Text = "Score: " + counter;
+                }));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Left) 
+            {
+                if (airCraft.Location.X - 50 > -1) {
+                    airCraft.Location = new Point(airCraft.Location.X - 50, airCraft.Location.Y);
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (airCraft.Location.X + 50 < 800) {
+                    airCraft.Location = new Point(airCraft.Location.X + 50, airCraft.Location.Y);
+                }
+            }
+        }
+
+        private void generateAsteroids()
+        {
+            Random random = new Random();
+            int i = 0;
+            while (!stopThread)
+            {
+                Thread.Sleep(2000);
+
+                //this.Invoke(new Action(() =>
+                //{
+                //    ++counter;
+                //    score.Text = "Score: " + counter;
+                //}));
+
+                int randomXPosition = random.Next(0, 16) * 50;
+
+                this.Invoke(new Action(() =>
+                {
+                    asteroids[i] = new PictureBox();
+                    asteroids[i].Image = Image.FromFile("D:/GitHub Projects/Airplane Fighter/asteroid.png");
+                    asteroids[i].Location = new Point(randomXPosition, 10);
+                    asteroids[i].Size = new Size(40, 40);
+                    asteroids[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                    asteroids[i].BackColor = Color.Transparent;
+                    this.Controls.Add(asteroids[i]);
+                    
+                }));
+
+                Thread asteroidThread = new Thread(() =>
+                {
+                    moveAsteroid(asteroids[--i]);
+                });
+                asteroidThread.Start();
+
+                ++i;
+            }
+        }
+
+
+        private void moveAsteroid(PictureBox asteroid)
+        {
+            while (asteroid.Top < this.Height && !stopMoveThread)
+            {
+                Thread.Sleep(10);
+
+                this.Invoke(new Action(() =>
+                {
+                    asteroid.Top += 10;
+                    if (asteroid.Location.X == airCraft.Location.X && (asteroid.Location.Y == airCraft.Location.Y - 40 ||
+                    asteroid.Location.Y == airCraft.Location.Y - 30 ||
+                    asteroid.Location.Y == airCraft.Location.Y - 20 ||
+                    asteroid.Location.Y == airCraft.Location.Y - 10 ||
+                    asteroid.Location.Y == airCraft.Location.Y ||
+                    asteroid.Location.Y == airCraft.Location.Y + 10 ||
+                    asteroid.Location.Y == airCraft.Location.Y + 20 ||
+                    asteroid.Location.Y == airCraft.Location.Y + 30 ||
+                    asteroid.Location.Y == airCraft.Location.Y + 40)) 
+                    {
+                        endGame();
+                    }
+                }));
+            }
+        }
+
+        private void endGame() {
+            stopMoveThread = true;
+            stopThread = true;
+            MessageBox.Show("Game over! Your score: " + counter, "EndGame", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Exit();
+        }
+    }
+}
